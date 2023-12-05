1be50a6a7e0ceb692cf0f6157bb21122293b56d9
harry <kim95175@gmail.com>
2023-12-05 20:30:06 +0900

    chapter 6

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, DM_Serif_Text } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+export const dmSerifText = DM_Serif_Text({
+    weight: ['400'],
+    subsets: ['latin'],
+});
